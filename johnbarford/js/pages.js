d4612544163112b87376b0f02bff52dfb14a6219
James Barford-Evans <jamesbarfordevans@gmail.com>
2018-02-07 09:06:14 +0000

    changed

--- johnbarford/js/pages.js
+++ johnbarford/js/pages.js
@@ -1,14 +1,12 @@
 (function imgBackground() {
-  if (document.getElementsByTagName('h1')[1] == undefined || document.getElementsByTagName('h1') == null) {
-  console.log('')
- }
-  else if(document.getElementsByTagName('h1')[1].textContent == 'Consultancy') {
+  if(document.getElementsByTagName('h1')[1].textContent == 'Consultancy') {
     document.querySelector('.main-splash').style.backgroundImage = "url('styles/background/consultancy-background.jpg')";
     document.querySelector('.main-list').style.backgroundColor = '#3B6AA3';
   }
   else {
     document.querySelector('.main-splash').style.backgroundImage = "url('styles/background/interim-background.jpg')";
     document.querySelector('.main-list').style.backgroundColor = '#295C5F';
+    document.querySelector('.client-logos-list').style.paddingTop = '40px';
   }
 })();
 
