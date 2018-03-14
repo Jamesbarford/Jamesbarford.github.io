743b72c55404c4a84c832025026c930966d77847
James Barford-Evans <jamesbarfordevans@gmail.com>
2018-03-14 19:35:03 +0000

    removed arrow down from all pages & all devices

--- johnbarford/js/scripts.js
+++ johnbarford/js/scripts.js
@@ -31,7 +31,6 @@ var MobileFix = {
   homeWrap: document.querySelector('.home-text-wrap'),
   homeOverlay: document.querySelector('.home-overlay'),
   coloumnWrap: document.querySelector('.column-wrapper'),
-  arrowDown: document.querySelector('.fa-angle-down'),
   orientationChange: function () {
     var self = this;
     // Cache window dimensions
@@ -57,9 +56,7 @@ var MobileFix = {
       }
       else if(this.mainSplash) {
         this.mainSplash.style.height = window.innerHeight + 'px';
-        console.log('kawabunga');
       }
-        this.arrowDown.style.display = 'none';
         MobileFix.orientationChange();
     }
   }
@@ -86,40 +83,3 @@ var imageBackground = {
   }
 };
 imageBackground.changeBackground();
-
-function scrollIt(destination, duration, easing, callback) {
-  if (duration === undefined) duration = 200;
-  var easings = {
-    linear: function linear(t) {
-      return t;
-    }
-  };
-
-  var start = window.pageYOffset;
-  var startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
-  var documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
-  var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
-  var destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
-  var destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
-
-  if ('requestAnimationFrame' in window === false) {
-    window.scroll(0, destinationOffsetToScroll);
-    return;
-  }
-
-  function scroll() {
-    var now = 'now' in window.performance ? performance.now() : new Date().getTime();
-    var time = Math.min(1, (now - startTime) / duration);
-    var timeFunction = easings[easing](time);
-    window.scroll(0, Math.ceil(timeFunction * (destinationOffsetToScroll - start) + start));
-
-    if (window.pageYOffset === destinationOffsetToScroll) {
-      return;
-    }
-    window.requestAnimationFrame(scroll);
-  }
-  scroll();
-}
-document.querySelector('.fa-angle-down').addEventListener('click', function () {
-  scrollIt(document.querySelector('.column-wrapper') || document.querySelector('.page-image-wrap'), 500, 'linear')
-});
